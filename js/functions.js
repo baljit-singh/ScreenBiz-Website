df703f1f952e0f33ea8c66d8923e12affb0ec47a
baljit singh <baljitsinghx01@gmail.com>
2017-05-23 02:41:25 +0530

    added our tea, section

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,11 @@
 var height= $(window).height() - 200;
 $(".FirstSection").css("min-height",height);
-$(".SecondSection").css("min-height",$(window).height() - 320);
-$(".BrandSection").css("min-height",$(window).height() - 70);
-$(".whyUsSection").css("min-height",$(window).height() - 70);
+$(".SecondSection").css("min-height",$(window).height() - 240);
+$(".BrandSection").css("min-height",$(window).height() - 40);
+$(".whyUsSection").css("min-height",$(window).height() - 40);
 $(".fourthSection").css("min-height",height);
 $(".quoteSection").css("min-height",$(window).height() - 490);
+$(".ourTeam").css("min-height",$(window).height() - 150);
 $(".locationScr").css("min-height",height);
 $("footer").css("min-height",$(window).height() - 500);
 
@@ -27,9 +28,18 @@ $("footer").css("min-height",$(window).height() - 500);
         .trigger('resize');
 })(jQuery);
 
+$('.mobilePhone a').on('click', function(){
+    // console.log($(this));
+    if($(this).attr('id') == 'iphone5'){
+        $('.MobilePic').addClass('imageOne');
+        // console.log("yes");
+    }else{
+        console.log("no");
+    }
+});
+
 
 // $('.selectpicker').selectpicker({
 //   style: 'btn-info',
 //   size: 4
 // });
-
